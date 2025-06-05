fbd35d6196178581f201fed965818410c4fc7742
joarezw68 <joarezw68@gmail.com>
2025-06-05 20:14:27 -0300

    Adicionando schema de mensagem

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,11 +2,20 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI(title="*** FastAPI - JW68 ***")
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
+
+
+@app.post(
+    '/users/', 
+    status_code=HTTPStatus.CREATED,
+    response_model=UserPublic
+    )  
+def create_user(user: UserSchema):
+    return user
