81f6d2bd24d71999068e99a5cfb64e7fff04ed69
joarezw68 <joarezw68@gmail.com>
2025-08-27 17:07:59 -0300

    atualizado na mao - jw68

---
+++ fastapi_zero/app copy.py
@@ -0,0 +1,32 @@
+from http import HTTPStatus
+
+from fastapi import FastAPI
+
+from fastapi_zero.schemas import (
+    Message, UserDB, UserList, UserPublic, UserSchema
+)
+app = FastAPI(title="*** FastAPI - JW68 ***")
+database = []
+
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {"message": "Olá mundo!"}
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1,
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
